71fc256512fc9236c8d890ee7b13d83ac9dc153d
Indra Saepudin <indrasaepudin212@gmail.com>
2025-01-23 19:29:00 +0700

    if statements

---
+++ ifstatement.js
@@ -0,0 +1,8 @@
+let angka = Math.random();
+console.log(angka);
+
+if (angka < 0.5) {
+  console.log("angka kurang dari 0.5");
+} else {
+  console.log("angka lebih dari 0.5");
+}
---
+++ string.js
@@ -0,0 +1,5 @@
+let hero = "barangaroo";
+let hero2 = "jokonong";
+
+console.table(hero); // 9
+// console.log(hero2.length); // 8
