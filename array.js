2c2a52729d3bf68f375282af6e806a2b329ef1a8
Indra Saepudin <indrasaepudin212@gmail.com>
2025-01-23 22:31:40 +0700

    Array Manipulation

--- array.js
+++ array.js
@@ -1,5 +1,10 @@
+// Array adalah tipe data yang digunakan untuk menampung lebih dari satu nilai. Nilai-nilai tersebut dapat berupa tipe data apapun, seperti number, string, boolean, object, dan sebagainya. Array dideklarasikan dengan menggunakan tanda kurung siku ([]) dan setiap nilai dipisahkan dengan tanda koma (,).
 let peserta = ["A", "B", "C", "D", "E"];
 let nilai = [70, 80, 90, 85, 75];
 let music = ["rock", "pop", "jazz", "dangdut", "kpop"];
 
+music['rock'] = "jpop";
+music[5] = "malaysia";
+
 console.log(music.length);
+console.log(music);
