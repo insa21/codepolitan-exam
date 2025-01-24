ee2d5a56b1c6030c4f6865d8c5e4b7cd2ac769c6
Indra Saepudin <indrasaepudin212@gmail.com>
2025-01-24 17:24:39 +0700

    Object

---
+++ object.js
@@ -0,0 +1,29 @@
+// Obeject
+tinggi = 170;
+const fitnessData = {
+  weight: tinggi,
+  height: 67,
+  age: 25,
+};
+
+console.log(fitnessData.height);
+
+fitnessData.height = 132;
+
+console.log(fitnessData.weight);
+console.log(fitnessData.height);
+console.log(fitnessData.age);
+
+const saya = {
+  nama: "Indra",
+  umur: 24,
+  pekerjaan: "Programmer",
+};
+
+console.log(saya.nama);
+console.log(saya.umur);
+console.log(saya.pekerjaan);
+
+saya.nama = "Indra permana";
+
+console.log(saya.nama);
