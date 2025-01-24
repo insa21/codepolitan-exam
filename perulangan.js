50586aa252a56e2d30c7abd5b8c95ee57e81dbef
Indra Saepudin <indrasaepudin212@gmail.com>
2025-01-24 17:45:46 +0700

    perulangan For

---
+++ perulangan.js
@@ -0,0 +1,25 @@
+// 1. Perulangan For
+// menghitung bilangan dari 1 sampai 10
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// Menghitung bilangan genap dari 1 sampai 50
+// for (let i = 2; i <= 50; i+=2) {
+//   console.log(i);
+// }
+
+// Menghitung bilangan ganjil dari 1 sampai 50
+// for (let i = 1; i <= 50; i+=2) {
+//   console.log(i);
+// }
+
+// Menghitung bilangan kelipatan 5 dari 1 sampai 50
+// for (let i = 5; i <= 50; i+=5) {
+//   console.log(i);
+// }
+
+// Menghitung bilangan mundur dari 10 sampai 1
+// for (let i = 10; i >= 1; i--) {
+//   console.log(i);
+// }
