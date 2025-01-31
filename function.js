7daab24b8cc3165c8202e0d3b8683cb16a7a690d
Indra Saepudin <indrasaepudin212@gmail.com>
2025-01-31 23:32:37 +0700

    Function Js

--- function.js
+++ function.js
@@ -6,6 +6,7 @@ function lemparDadu() {
 // console.log(lemparDadu());
 lemparDadu();
 
+console.log("=============================================");
 // parameter dan argument pada function greet
 function greet(name) {
   console.log(`Hello, ${name}!`);
@@ -13,9 +14,42 @@ function greet(name) {
 greet("John");
 
 function cuaca(kota, hari, cuaca) {
+  // return(`Cuaca di ${kota} pada hari ${hari} adalah ${cuaca}`);
   console.log(`Cuaca di ${kota} pada hari ${hari} adalah ${cuaca}`);
 }
 
-cuaca("Jakarta", "senin", "cerah");
+// console.log(cuaca("Jakarta", "senin", "cerah"));
 cuaca("Bandung", "selasa", "hujan");
 cuaca("Bali", "rabu", "berawan");
+
+console.log("=============================================");
+// function dengan return value
+function matematika(a, b) {
+  const tambah = a + b;
+  console.log(tambah);
+  const kurang = a - b;
+  console.log(kurang);
+  const kali = a * b;
+  console.log(kali);
+  const bagi = a / b;
+  console.log(bagi);
+  const pangkat = a ** b;
+  console.log(pangkat);
+  const modulus = a % b;
+  console.log(modulus);
+}
+
+matematika(10, 20);
+
+console.log("=============================================");
+
+// Statement return pada function
+
+function jumlahkan(a, b) {
+  if (typeof a !== "number" || typeof b !== "number") {
+    return "Input harus number";
+  }
+  return a + b;
+}
+
+console.log(jumlahkan('hdhhd'));
