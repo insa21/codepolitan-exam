9842ce9058c2b1cd6812b6a0a8fedba9f155b9a0
Indra Saepudin <indrasaepudin212@gmail.com>
2025-01-31 19:50:28 +0700

    For Of

---
+++ for_of.js
@@ -0,0 +1,14 @@
+// const fruits = ["apple", "banana", "orange", "grape", "kiwi"];
+
+
+// todo: Cara biasa
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(`I like ${fruits[i]}`);
+// }
+
+// todo: menggunakan for of
+for (const fruit of fruits) {
+  console.log(`I like ${fruit}`);
+}
+
+
