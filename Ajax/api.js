99039b34841f99f06f8d74ecdbb7c5283f8d42af
Indra Saepudin <indrasaepudin212@gmail.com>
2025-02-12 12:40:01 +0700

    Melakukan Request Dengan Fetch dan Error Handling

--- Ajax/api.js
+++ Ajax/api.js
@@ -14,20 +14,50 @@
 // }`;
 
 // Request dengan XMLHttpRequest
-const req = new XMLHttpRequest();
-let data;
+// const req = new XMLHttpRequest();
+// let data;
 
-req.onload = function () {
-  data = JSON.parse(this.responseText);
-  console.log(data);
-};
+// req.onload = function () {
+//   data = JSON.parse(this.responseText);
+//   console.log(data);
+// };
 
-req.onerror = function () {
-  console.log("Error", this);
-};
+// req.onerror = function () {
+//   console.log("Error", this);
+// };
 
-req.open("GET", "https://swapi.dev/api/people/1/");
-req.send();
-
+// req.open("GET", "https://swapi.dev/api/people/1/");
+// req.send();
 
 //Fetch
+// fetch("https://swapi.dev/api/people/1/")
+//   .then((res) => {
+//     if (!res.ok) {
+//       throw Error(`Could not fetch the data for that resource`);
+//     }
+//     return res.json();
+//   })
+
+//   .then((data) => {
+//     console.log("json", data);
+//   })
+
+//   .catch((err) => {
+//     console.log("Error", err);
+//   });
+
+const loadPeople = async () => {
+  try {
+    const res = await fetch("https://swapi.dev/api/people/1/");
+    const data = await res.json();
+    console.log(data);
+
+    const res2 = await fetch("https://swapi.dev/api/people/2/");
+    const data2 = await res2.json();
+    console.log(data2);
+  } catch (err) {
+    console.log("Error nihh ganti-ganti pesannya:", err);
+  }
+};
+
+loadPeople();
