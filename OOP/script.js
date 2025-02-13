9533c14c253355f28c74b7fb0b0b66619a9881f8
Indra Saepudin <indrasaepudin212@gmail.com>
2025-02-13 12:24:27 +0700

    Membuat Object Dengan Class

--- OOP/script.js
+++ OOP/script.js
@@ -1,27 +1,88 @@
-const hex = (r, g, b) => {
-  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-};
+// Factory Function
+// const hex = (r, g, b) => {
+//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
 
-const rgb = (r, g, b) => {
-  return `rgb(${r}, ${g}, ${b})`;
-};
+// const rgb = (r, g, b) => {
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
 
-function convertColor(r, g, b) {
-  const color = {};
+// function convertColor(r, g, b) {
+//   const color = {};
 
-  color.r = r;
-  color.g = g;
-  color.b = b;
+//   color.r = r;
+//   color.g = g;
+//   color.b = b;
 
-  color.rgb = function () {
+//   color.rgb = function () {
+//     const { r, g, b } = this;
+//     return `rgb(${r}, ${g}, ${b})`;
+//   };
+
+//   color.hex = function () {
+//     const { r, g, b } = this;
+//     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+//   };
+
+//   return color;
+// }
+
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// Consturcktor Function
+// function Color(r, g, b) {
+//   this.r = r;
+//   this.g = g;
+//   this.b = b;
+// }
+
+// Color.prototype.rgb = function () {
+//   const { r, g, b } = this;
+//   return `rgb(${r}, ${g}, ${b})`;
+// };
+
+// Color.prototype.hex = function () {
+//   const { r, g, b } = this;
+//   return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+// };
+
+// Color.prototype.rgba = function (a = 1.0) {
+//   const { r, g, b } = this;
+//   return `rgba(${r}, ${g}, ${b}, ${a})`;
+// };
+
+class Color {
+  constructor(r, g, b, name) {
+    this.r = r;
+    this.g = g;
+    this.b = b;
+    this.name = name;
+
+    console.log("Hi, ini adlaah proses constructor");
+    console.log(`${r}`);
+  }
+
+  colorName() {
+    console.log("The color name is" + this.name);
+  }
+
+  rgb() {
     const { r, g, b } = this;
     return `rgb(${r}, ${g}, ${b})`;
-  };
+  }
 
-  color.hex = function () {
+  rgba(a = 1.0) {
+    const { r, g, b } = this;
+    return `rgba(${r}, ${g}, ${b}, ${a})`;
+  }
+
+  hex() {
     const { r, g, b } = this;
     return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-  };
-
-  return color;
+  }
 }
+new Color();
