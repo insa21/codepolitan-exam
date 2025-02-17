b7977dd8597feed2991a054f4b18082841360867
Indra Saepudin <indrasaepudin212@gmail.com>
2025-02-17 23:43:36 +0700

    Persiapan Project Untuk Pelajari Component Dan Props

---
+++ Vue/quiz-vue/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <main>
+    <header>
+      <h1 id="title">Quiz vue</h1>
+      <input type="text" id="search-input">
+    </header>
+  </main>
+</template>
+
+
+<style scoped>
+main {
+  max-width: 900px;
+  margin: 0 auto;
+}
+
+header {
+  margin-top: 30px;
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+}
+
+#title {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+#search-input {
+  border: none;
+  background-color: #c9c9c9a9;
+  padding: 10px;
+  border-radius: 5px;
+}
+
+
+</style>
