83e11ceddc151d29db98a6a3c9b4a0d9f7ce6a20
Indra Saepudin <indrasaepudin212@gmail.com>
2025-02-20 14:24:34 +0700

    Belajar RESTful API dengan Express JS

--- expressJS/RESTfull-API/rest/index.js
+++ expressJS/RESTfull-API/rest/index.js
@@ -1,14 +1,21 @@
 const path = require("path");
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
+const methodOverride = require("method-override");
 const app = express();
 
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
 app.use(express.urlencoded({ extended: true }));
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-const comments = [
+let comments = [
   {
+    id: uuidv4(),
     name: "John",
     comment: "heloo broo, menarik nihhh",
     date: "2021-01-01",
@@ -18,6 +25,7 @@ const comments = [
     dislikes: 2,
   },
   {
+    id: uuidv4(),
     name: "Jane",
     comment: "This is a comment",
     date: "2021-01-01",
@@ -27,6 +35,7 @@ const comments = [
     dislikes: 2,
   },
   {
+    id: uuidv4(),
     name: "jony",
     comment: "komennnn",
     date: "2021-01-01",
@@ -36,6 +45,7 @@ const comments = [
     dislikes: 2,
   },
   {
+    id: uuidv4(),
     name: "jeny",
     comment: "no commennt",
     date: "2021-01-01",
@@ -48,6 +58,42 @@ const comments = [
 
 app.get("/comments", (req, res) => {
   res.render("comments/index", { comments });
+});
+
+app.get("/comments/create", (req, res) => {
+  res.render("comments/create");
+});
+
+app.post("/comments", (req, res) => {
+  const { likes, name, comment } = req.body;
+  comments.push({ likes, name, comment, id: uuidv4() });
+  res.redirect("comments");
+});
+
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
+});
+
+app.get("/comments/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/edit", { comment });
+});
+
+app.delete("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  comments = comments.filter((c) => c.id !== id);
+  res.redirect("/comments");
+});
+
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newComment = req.body.comment;
+  const foundComment = comments.find((c) => c.id === id);
+  foundComment.comment = newComment;
+  res.redirect("/comments");
 });
 
 app.get("/", (req, res) => {
