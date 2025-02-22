5efb5e8d8685cd38244fd084bf0bd843f8cebf01
Indra Saepudin <indrasaepudin212@gmail.com>
2025-02-22 18:49:40 +0700

    Menjalankan Logic Berdasarkan Perubahan State Atau Data Dengan Watch dan mengambil data dari Json

--- Vue/quiz-vue/src/App.vue
+++ Vue/quiz-vue/src/App.vue
@@ -1,9 +1,16 @@
 <script setup>
 import srcQuiz from "./data/quizes.json"
-import { ref } from "vue";
+import { ref, watch } from "vue";
 
 const quizes = ref(srcQuiz);
+const search = ref("");
 console.log(quizes)
+
+watch(search, () => {
+  quizes.value = srcQuiz.filter(quiz => {
+    return quiz.title.toLowerCase().includes(search.value.toLowerCase());
+  });
+})
 
 
 </script>
@@ -12,19 +19,16 @@ console.log(quizes)
   <main>
     <header>
       <h1 id="title">Quiz vue</h1>
-      <input type="text" id="search-input">
+      <input v-model.trim="search" type="text" id="search-input">
     </header>
     <section id="quiz-container">
       <div v-for="quiz in quizes" :key="quiz.id" class="card">
-        <img
-          :src="quiz.img"
-          :alt="quiz.title">
-        <div class="body-card">
+        <img :src="quiz.img" alt="image" srcset="">
+        <div class="card-body">
           <h2>{{ quiz.title }}</h2>
-          <p>{{ quiz.question.length }}Questions</p>
+          <p>{{ quiz.questions.length }}Questions</p>
         </div>
       </div>
-
     </section>
   </main>
 </template>
@@ -54,5 +58,41 @@ header {
   background-color: #c9c9c9a9;
   padding: 10px;
   border-radius: 5px;
+}
+
+
+#quiz-container {
+  display: flex;
+  flex-wrap: 20px;
+  margin-top: 20px;
+
+}
+
+
+.card {
+  width: 200px;
+  margin-right: 20px;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: 0.3s;
+  cursor: pointer;
+}
+
+img {
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  object-position: center;
+}
+
+.card-body {
+  padding: 0 15px;
+}
+
+
+.card-body h2 {
+  font-weight: bold;
+
 }
 </style>
