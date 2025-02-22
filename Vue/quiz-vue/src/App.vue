ee67795c9e2a25bbd426d9292ef55baaa7d50a29
Indra Saepudin <indrasaepudin212@gmail.com>
2025-02-22 23:44:55 +0700

    Router Vue

--- Vue/quiz-vue/src/App.vue
+++ Vue/quiz-vue/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import srcQuiz from "./data/quizes.json"
 import { ref, watch } from "vue";
+import QuizCard from "./components/QuizCard.vue";
 
 const quizes = ref(srcQuiz);
 const search = ref("");
@@ -22,13 +23,14 @@ watch(search, () => {
       <input v-model.trim="search" type="text" id="search-input">
     </header>
     <section id="quiz-container">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <QuizCard v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="image" srcset="">
         <div class="card-body">
           <h2>{{ quiz.title }}</h2>
           <p>{{ quiz.questions.length }}Questions</p>
         </div>
-      </div>
+      </div> -->
     </section>
   </main>
 </template>
@@ -65,34 +67,5 @@ header {
   display: flex;
   flex-wrap: 20px;
   margin-top: 20px;
-
-}
-
-
-.card {
-  width: 200px;
-  margin-right: 20px;
-  border-radius: 5px;
-  overflow: hidden;
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-  transition: 0.3s;
-  cursor: pointer;
-}
-
-img {
-  width: 100%;
-  height: 150px;
-  object-fit: cover;
-  object-position: center;
-}
-
-.card-body {
-  padding: 0 15px;
-}
-
-
-.card-body h2 {
-  font-weight: bold;
-
 }
 </style>
---
+++ Vue/quiz-vue/src/components/QuizCard.vue
@@ -0,0 +1,43 @@
+<script setup>
+const { props } = defineProps(["quiz"]);
+console.log(props);
+
+</script>
+
+
+<template>
+  <div class="card">
+    <img :src="quiz.img" alt="quiz.title" :quizes="quizes">
+    <div class="card-body">
+      <h2>{{ quiz.title }}</h2>
+      <p>{{ quiz.questions.length }} Questions</p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.card {
+  width: 200px;
+  margin-right: 20px;
+  border-radius: 5px;
+  overflow: hidden;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  transition: 0.3s;
+  cursor: pointer;
+}
+
+img {
+  width: 100%;
+  height: 150px;
+  object-fit: cover;
+  object-position: center;
+}
+
+.card-body {
+  padding: 0 15px;
+}
+
+.card-body h2 {
+  font-weight: bold;
+}
+</style>
