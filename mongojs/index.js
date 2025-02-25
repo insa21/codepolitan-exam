55dd88163c2896a818a6aa1e00ce44d5dd104ec6
Indra Saepudin <indrasaepudin212@gmail.com>
2025-02-25 23:21:44 +0700

    Insert Data to DB

--- mongojs/index.js
+++ mongojs/index.js
@@ -11,21 +11,77 @@ mongoose
 const movieSchema = new mongoose.Schema({
   title: String,
   year: Number,
-  rating: Number,
+  genre: String,
   director: String,
   actor: [String],
+  rating: Number,
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
 
-const movie = new Movie({
-  title: "Black Panther",
-  year: 2018,
-  score: 8.7,
-  director: "Ryan Coogler",
-  actor: ["jono", "joni"],
-});
+// const movie = new Movie({
+//   title: "Black Panther",
+//   year: 2018,
+//   genre: "Action",
+//   director: "Ryan Coogler",
+//   actor: ["jono", "joni"],
+//   ratting: 8.7,
+// });
 
-movie.save();
+// movie.save();
 
-console.log(movie);
+// console.log(movie);
+
+Movie.insertMany([
+  {
+    title: "Avengers: Infinity War",
+    genre: "Action",
+    director: "Anthony Russo, Joe Russo",
+    year: 2018,
+    cast: ["Robert Downey Jr.", "Chris Hemsworth", "Mark Ruffalo"],
+    description:
+      "The Avengers and their allies must be willing to sacrifice all in an attempt to defeat the powerful Thanos before his blitz of devastation and ruin puts an end to the universe.",
+    rating: 8.4,
+    image: "https://www.example.com/avengers_infinity_war.jpg",
+  },
+  {
+    title: "Joker",
+    genre: "Crime",
+    director: "Todd Phillips",
+    year: 2019,
+    cast: ["Joaquin Phoenix", "Robert De Niro", "Zazie Beetz"],
+    description:
+      "In Gotham City, mentally-troubled comedian Arthur Fleck is disregarded and mistreated by society. He then embarks on a downward spiral of revolution and bloody crime. This path brings him face-to-face with his alter-ego: 'The Joker'.",
+    rating: 8.4,
+    image: "https://www.example.com/joker.jpg",
+  },
+  {
+    title: "Parasite",
+    genre: "Drama",
+    director: "Bong Joon Ho",
+    year: 2019,
+    cast: ["Song Kang-ho", "Lee Sun-kyun", "Cho Yeo-jeong"],
+    description:
+      "Greed and class discrimination threaten the newly formed symbiotic relationship between the wealthy Park family and the destitute Kim clan.",
+    rating: 8.6,
+    image: "https://www.example.com/parasite.jpg",
+  },
+  {
+    title: "Spider-Man: Into the Spider-Verse",
+    genre: "Animation",
+    director: "Bob Persichetti, Peter Ramsey, Rodney Rothman",
+    year: 2018,
+    cast: ["Shameik Moore", "Jake Johnson", "Hailee Steinfeld"],
+    description:
+      "Teen Miles Morales becomes the Spider-Man of his universe, and must join with five spider-powered individuals from other dimensions to stop a threat for all realities.",
+    rating: 8.4,
+    image: "https://www.example.com/spider_man_into_the_spider_verse.jpg",
+  },
+])
+  .then((movies) => {
+    console.log("Movies inserted successfully");
+    console.log(movies);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
